0e72bcf243b07245929c4030e99184437eb3d10d
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-01-30 21:27:48 -0300

    chore: add ts

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,27 @@ const LiveReloadPlugin = require('webpack-livereload-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/frontend/index.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist/public'),
     filename: 'bundle_client.js',
     clean: true
   },
   module: {
-    rules: [{
-      test: /.js/,
-      use: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /.js/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new LiveReloadPlugin()
--- webpack.server.js
+++ webpack.server.js
@@ -10,17 +10,28 @@ npx nodemon dist/bundle_server.js
 */
 
 module.exports = {
-  entry: './src/backend/server.js',
+  entry: './src/backend/server.tsx',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle_server.js'
   },
   target: 'node',
   module: {
-    rules: [{
-      test: /.js/,
-      use: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /.js/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   externals: [
     nodeExternals()
