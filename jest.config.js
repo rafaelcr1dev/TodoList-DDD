dc22bdd2c8ef97bbbb2383ae50682ac746a47165
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-01-30 23:18:13 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/tests/unit'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/config/jest-setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/tests/e2e'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/unit$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+}
---
+++ tests/unit/exemple.spec.ts
@@ -0,0 +1,5 @@
+describe('Name ....', () => {
+  test('Should sum two numbers 1 + 3 and equal 4', () => {
+    expect(1 + 3).toBe(4)
+  })
+})
