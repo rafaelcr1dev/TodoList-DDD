164e8fbc8773f72723023e35b9d494663f38e74f
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-02-28 15:48:50 -0300

    test: ensure return null AddTodoRepository

--- tests/unit/frontend/data/mocks/mock-todo.ts
+++ tests/unit/frontend/data/mocks/mock-todo.ts
@@ -1,20 +1,21 @@
-import { AddTodoRepository } from '@/frontend/data/protocols/cache/todo'
+import faker from '@faker-js/faker'
 import { v4 as uuidv4 } from 'uuid'
+
+import { AddTodoRepository } from '@/frontend/data/protocols/cache/todo'
 
 export class AddTodoRepositorySpy implements AddTodoRepository {
   nonce: number = 0
   params: AddTodoRepository.Params
-  result: AddTodoRepository.Result
+  result: AddTodoRepository.Result = {
+    todoId: uuidv4(),
+    todoName: faker.name.findName()
+  }
 
   async add(
     params: AddTodoRepository.Params
   ): Promise<AddTodoRepository.Result> {
     this.nonce = this.nonce + 1
     this.params = params
-    this.result = {
-      todoId: uuidv4(),
-      todoName: params.todoName
-    }
 
     return this.result
   }
