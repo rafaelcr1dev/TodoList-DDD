12344c332478955ca1d6bdb9650e866e41827d56
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 14:44:30 -0300

    feat: ensure call add with correct values

--- src/frontend/infra/cache/local-storage/todo-local-storage-repository.ts
+++ src/frontend/infra/cache/local-storage/todo-local-storage-repository.ts
@@ -1,5 +1,9 @@
-export class TodoLocalStorageRepository {
-  add(): any {
-    return {}
+import { AddTodoRepository } from '@/frontend/data/protocols/cache/todo'
+
+export class TodoLocalStorageRepository implements AddTodoRepository {
+  async add(
+    params: AddTodoRepository.Params
+  ): Promise<AddTodoRepository.Result> {
+    return Promise.resolve(params)
   }
 }
