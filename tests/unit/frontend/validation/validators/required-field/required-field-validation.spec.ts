4ed9725f7e8fe9bcb4f3937f72237b06b7bb2beb
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 18:49:22 -0300

    test: ensure be exists RequiredFieldValidation

---
+++ tests/unit/frontend/validation/validators/required-field/required-field-validation.spec.ts
@@ -0,0 +1,7 @@
+import { RequiredFieldValidation } from '@/frontend/validation/validators/required-field/required-field-validation'
+
+describe('RequiredFieldValidation Smoke Tests', () => {
+  test('Should be exists RequiredFieldValidation', () => {
+    expect(RequiredFieldValidation).toBeDefined()
+  })
+})
