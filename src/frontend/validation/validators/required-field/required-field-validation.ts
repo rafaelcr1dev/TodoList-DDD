982c02fcad259d26d3ab97126cf93287785fc5b5
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 19:03:10 -0300

    feat: ensure be exists call validate of RequiredFieldValidation

--- src/frontend/validation/validators/required-field/required-field-validation.ts
+++ src/frontend/validation/validators/required-field/required-field-validation.ts
@@ -1,1 +1,6 @@
-export class RequiredFieldValidation {}
+import { FieldValidation } from '../../protocols/field-validation'
+
+export class RequiredFieldValidation implements FieldValidation {
+  field: string
+  validate(): any {}
+}
