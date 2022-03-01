f1fbeb955a9274ca8f9c554b8dccb6a9c3dce309
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 20:45:41 -0300

    test: ensure be exists call required of ValidationBuilder

--- tests/unit/frontend/validation/validators/builder/validation-builder.spec.ts
+++ tests/unit/frontend/validation/validators/builder/validation-builder.spec.ts
@@ -1,3 +1,4 @@
+import faker from '@faker-js/faker'
 import { ValidationBuilder } from '@/frontend/validation/validators/builder/validation-builder'
 
 describe('ValidationBuilder Smoke Tests', () => {
@@ -7,5 +8,11 @@ describe('ValidationBuilder Smoke Tests', () => {
 
   test('Should be exists call field of ValidationBuilder', () => {
     expect(ValidationBuilder.field).toBeDefined()
+  })
+
+  test('Should be exists call required of ValidationBuilder', () => {
+    expect(
+      ValidationBuilder.field(faker.database.column()).required
+    ).toBeDefined()
   })
 })
