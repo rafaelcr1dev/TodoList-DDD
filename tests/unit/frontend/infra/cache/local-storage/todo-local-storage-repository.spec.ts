006103a59d55e3e8bb17c32f77b06d82969f4432
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 14:33:01 -0300

    test: add makeSut

--- tests/unit/frontend/infra/cache/local-storage/todo-local-storage-repository.spec.ts
+++ tests/unit/frontend/infra/cache/local-storage/todo-local-storage-repository.spec.ts
@@ -1,11 +1,23 @@
 import { TodoLocalStorageRepository } from '@/frontend/infra/cache/local-storage/todo-local-storage-repository'
 
+type SutTypes = {
+  sut: TodoLocalStorageRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new TodoLocalStorageRepository()
+  return {
+    sut
+  }
+}
+
 describe('TodoLocalStorage Smoke test Repository', () => {
   test('Should be exists TodoLocalStorageRepository', () => {
     expect(TodoLocalStorageRepository).toBeDefined()
   })
 
   test('Should be exists call method add TodoLocalStorageRepository', () => {
-    expect(new TodoLocalStorageRepository().add).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut.add).toBeDefined()
   })
 })
