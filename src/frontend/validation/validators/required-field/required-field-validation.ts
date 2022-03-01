6f0d40fa0d0244b506778e45edd83d267e7c3f09
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 19:18:33 -0300

    feat: ensure call RequiredFieldValidation with correct values

--- src/frontend/validation/validators/required-field/required-field-validation.ts
+++ src/frontend/validation/validators/required-field/required-field-validation.ts
@@ -1,6 +1,7 @@
 import { FieldValidation } from '../../protocols/field-validation'
 
 export class RequiredFieldValidation implements FieldValidation {
-  field: string
-  validate(): any {}
+  constructor(readonly field: string) {}
+
+  validate(input: object): any {}
 }
