4b3dd6fc970cce448aa9e649eba3cb54fdad1833
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 19:19:51 -0300

    test: ensure call RequiredFieldValidation with correct values

--- tests/unit/frontend/validation/validators/required-field/required-field-validation.spec.ts
+++ tests/unit/frontend/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,4 +1,6 @@
 import { RequiredFieldValidation } from '@/frontend/validation/validators/required-field/required-field-validation'
+const makeSut = (field: string): RequiredFieldValidation =>
+  new RequiredFieldValidation(field)
 
 describe('RequiredFieldValidation Smoke Tests', () => {
   test('Should be exists RequiredFieldValidation', () => {
@@ -6,6 +8,21 @@ describe('RequiredFieldValidation Smoke Tests', () => {
   })
 
   test('Should be exists call validate of RequiredFieldValidation', () => {
-    expect(new RequiredFieldValidation().validate).toBeDefined()
+    const sut = makeSut('name')
+    expect(sut.validate).toBeDefined()
+  })
+})
+
+describe('RequiredField Validation', () => {
+  test('Should call RequiredFieldValidation with correct values', () => {
+    const sut = makeSut('name')
+    const validateSpy = jest.spyOn(sut, 'validate')
+
+    const input = {
+      name: 'Any Name Todo'
+    }
+
+    sut.validate(input)
+    expect(validateSpy).toHaveBeenCalledWith(input)
   })
 })
