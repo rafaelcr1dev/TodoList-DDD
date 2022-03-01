63d5a1f88e31fdac349554f0c85734c6e39a5418
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 20:18:57 -0300

    test: ensure be exists ValidationBuilder

---
+++ tests/unit/frontend/validation/validators/builder/validation-builder.spec.ts
@@ -0,0 +1,7 @@
+import { ValidationBuilder } from '@/frontend/validation/validators/builder/validation-builder'
+
+describe('ValidationBuilder Smoke Tests', () => {
+  test('Should be exists ValidationBuilder', () => {
+    expect(ValidationBuilder).toBeDefined()
+  })
+})
