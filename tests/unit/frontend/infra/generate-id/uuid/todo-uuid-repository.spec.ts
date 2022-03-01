f11960736e7afdba1c857c2ddc62ff62c333954f
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 12:23:54 -0300

    test: add makeSut on TodoUUIDRepository

--- tests/unit/frontend/infra/generate-id/uuid/todo-uuid-repository.spec.ts
+++ tests/unit/frontend/infra/generate-id/uuid/todo-uuid-repository.spec.ts
@@ -1,15 +1,33 @@
 import { TodoUUIDRepository } from '@/frontend/infra/generate-id'
 
+type SutTypes = {
+  sut: TodoUUIDRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new TodoUUIDRepository()
+
+  return {
+    sut
+  }
+}
+
 describe('TodoUUID Smoke Test Repository', () => {
   test('Should be exists TodoUUIDRepository', () => {
     expect(TodoUUIDRepository).toBeDefined()
   })
 
   test('Should be exists call method generate TodoUUIDRepository', () => {
-    expect(new TodoUUIDRepository().generate).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut.generate).toBeDefined()
   })
 
   test('Should be exists call method validate TodoUUIDRepository', () => {
-    expect(new TodoUUIDRepository().validate).toBeDefined()
+    const { sut } = makeSut()
+    expect(sut.validate).toBeDefined()
   })
+})
+
+describe('TodoUUID Repository', () => {
+  describe('Generate', () => {})
 })
