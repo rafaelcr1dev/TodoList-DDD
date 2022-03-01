e8c71ba61d9c2362669bf7f5f2a4eb7babc838e1
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 12:02:17 -0300

    feat: ensure be exists TodoUUIDRepository

---
+++ src/frontend/infra/generate-id/uuid/todo-uuid-repository.ts
@@ -0,0 +1,7 @@
+import { GenerateIdRepository } from '@/frontend/data/protocols/cache/todo'
+
+export class TodoUUIDRepository implements GenerateIdRepository {
+  generate(): any {
+    return null
+  }
+}
