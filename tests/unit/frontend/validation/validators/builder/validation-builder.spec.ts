df7ea168b0f37499565aaecc20b0644ae29eb701
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-01 21:16:01 -0300

    test: ensure return RequiredFieldValidation

--- tests/unit/frontend/validation/validators/builder/validation-builder.spec.ts
+++ tests/unit/frontend/validation/validators/builder/validation-builder.spec.ts
@@ -1,18 +1,25 @@
 import faker from '@faker-js/faker'
-import { ValidationBuilder } from '@/frontend/validation/validators/builder/validation-builder'
+import { ValidationBuilder as sut } from '@/frontend/validation/validators/builder/validation-builder'
+import { RequiredFieldValidation } from '@/frontend/validation/validators/required-field/required-field-validation'
 
 describe('ValidationBuilder Smoke Tests', () => {
   test('Should be exists ValidationBuilder', () => {
-    expect(ValidationBuilder).toBeDefined()
+    expect(sut).toBeDefined()
   })
 
   test('Should be exists call field of ValidationBuilder', () => {
-    expect(ValidationBuilder.field).toBeDefined()
+    expect(sut.field).toBeDefined()
   })
 
   test('Should be exists call required of ValidationBuilder', () => {
-    expect(
-      ValidationBuilder.field(faker.database.column()).required
-    ).toBeDefined()
+    expect(sut.field(faker.database.column()).required).toBeDefined()
+  })
+})
+
+describe('ValidationBuilder', () => {
+  test('Should return RequiredFieldValidation', () => {
+    const field = faker.database.column()
+    const validations = sut.field(field).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 })
