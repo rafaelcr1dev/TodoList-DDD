e849f7a65954cbf1f8f84ce913aa0e18b49bd44f
rafaelcr123 <rafaelcruz1@hotmail.com>
2022-03-27 22:17:52 -0300

    feat: add call load be called time once

--- src/frontend/data/usecases/todo/load-todos.ts
+++ src/frontend/data/usecases/todo/load-todos.ts
@@ -1,7 +1,11 @@
 import { LoadTodoProtocol } from '@/frontend/domain/usecases'
 
 export class LoadTodos implements LoadTodoProtocol {
+  constructor(private readonly loadTodosRepository: any) {}
+
   async load(): Promise<LoadTodoProtocol.Result[]> {
+    await this.loadTodosRepository.load()
+
     return []
   }
 }
